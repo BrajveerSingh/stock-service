01974a5f7aae805095ad7978a0fe846b8e59ce11
BrajveerSingh <brajveer.rana@gmail.com>
2024-04-10 19:43:20 +0530

    Initial commit

---
+++ src/main/java/com/study/stockservice/service/OrderConsumer.java
@@ -0,0 +1,22 @@
+package com.study.stockservice.service;
+
+import com.study.basedomains.dto.OrderEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+    @KafkaListener(
+            topics = "${spring.kafka.topic.name}",
+            groupId = "${spring.kafka.consumer.group-id}"
+    )
+    public void consumeOrder(final OrderEvent orderEvent) {
+        LOGGER.info("Consuming orderEvent: {}", orderEvent);
+        //persist the orderEvent to the database
+
+    }
+}
